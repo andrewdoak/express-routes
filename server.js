b388db34466432d1ca720f6136738fcf8e69a747
andrew <thedoak@gmail.com>
2023-08-31 11:37:20 -0500

    Refactor Eight Ball block after in-class code-along.

--- server.js
+++ server.js
@@ -38,6 +38,12 @@ app.get('/tip', function(req, res) {
 })
 app.get('/tip/:total/:tipPercentage', function(req, res) {
     // Create variables and convert strings to numbers
+    /* 
+    DESTRUCTURING
+    const {total, tipPercentage } = req.params
+    const tipAmount = (total * tipPercentage) / 100;
+    const tipPercentage = parseFloat(req.params.tipPercentage);
+    */
     const total = parseFloat(req.params.total);
     const tipPercentage = parseFloat(req.params.tipPercentage);
     const tipAmount = (total * tipPercentage) / 100;
@@ -51,14 +57,20 @@ app.get('/tip/:total/:tipPercentage', function(req, res) {
 
 
 // EightBall Array: Random Response
-app.get('/magic', function(req, res) {
+app.get('/magic/:question', function(req, res) {
     // Create responses array
     const responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"];
 
     // BIG UPS STEFON! (literally the last minute of our breakout rooms!)
     let randomResponse = responses[Math.floor(Math.random()*responses.length)]
-
-    res.send(`<h1>Will I be a Millionaire? <br> Magic Eight Ball Says: "${randomResponse}"</h1>`);
+    
+    // ${req.params.question}
+    // Instead of "Will I be a Millionaire?" 
+    res.send(`
+        <h1>${req.params.question}</h1> 
+        <br> 
+        <h3>Magic Eight Ball Says: "${randomResponse}"</h3>
+      `);
 });
 
 // Tell the app to listen on port 3000
